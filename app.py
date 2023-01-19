0a32634dba32e9f68c371f34c68221759ec51d9c
ariashahi3 <119522223@umail.ucc.ie>
2023-01-19 14:27:20 +0000

    login and signup start

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ application = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@ application.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@ application.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     application.debug = True
     application.run()
