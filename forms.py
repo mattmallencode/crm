293797a439c21243a27a73f902e1631daf37f49c
OliverLinger <olienew32@gmail.com>
2023-01-20 13:19:46 +0000

    Adding a length validator to the WTF forms.

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
-from wtforms.validators import InputRequired, EqualTo
+from wtforms.validators import InputRequired, EqualTo, Length
 
 class SignUpForm(FlaskForm):
     email = EmailField("Email:", validators=[
-                          InputRequired()], render_kw={"placeholder": "EMAIL"})
-    password = PasswordField("Password:", validators=[InputRequired()], render_kw={
+                          InputRequired(),Length(max=100)], render_kw={"placeholder": "EMAIL"})
+    password = PasswordField("Password:", validators=[InputRequired(),Length(max=100)], render_kw={
                              "placeholder": "PASSWORD"})
-    password2 = PasswordField("Confirm password:", validators=[InputRequired(), EqualTo("password")], render_kw={
+    password2 = PasswordField("Confirm password:", validators=[InputRequired(), EqualTo("password"),Length(max=100)], render_kw={
         "placeholder": "CONFIRM PASSWORD"})
     submit = SubmitField("REGISTER")
