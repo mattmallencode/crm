3556ef181b965b8caba25b6aa762235ef2cd7682
mattmallencode <72216955+mattmallencode@users.noreply.github.com>
2023-01-20 11:10:09 +0000

    Delete data_models directory
    Redundant.

--- data_models/users.py
+++
@@ -1,6 +0,0 @@
-class Users(db.Model):
-    email = db.Column(db.String, primary_key=True)
-    password_hash = db.Column(db.String)
-    team_id = db.Column(db.Integer)
-    owner_status = db.Column(db.Boolean)
-    admin_status = db.Column(db.Boolean)
