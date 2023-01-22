663cbb16a36d8923c064bd6f1fc0753b1189b40e
Matt Mallen <120355103@umail.ucc.ie>
2023-01-22 15:30:45 +0000

    Finished login flow, added login required functionality.

--- forms.py
+++ forms.py
@@ -12,10 +12,8 @@ class SignUpForm(FlaskForm):
     submit = SubmitField("REGISTER")
 
 class LoginForm(FlaskForm):
-    username = StringField("Username:", 
+    email = StringField("EMAIL:", 
         validators=[InputRequired(), Length(max=100)], render_kw={"placeholder": "USERNAME"})
     password = PasswordField("Password:", 
         validators=[InputRequired(),Length(max=100)], render_kw={"placeholder": "PASSWORD"})
     submit = SubmitField("LOGIN")
-
-    
