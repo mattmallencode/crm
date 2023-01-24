03a6424dd4345d0d46e8fc8decc556be01f86a80
OliverLinger <olienew32@gmail.com>
2023-01-24 17:20:48 +0000

    adding a tests folder

--- tests/test_login_register.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from application import *
-
-# Visit the login page 
-# Test login 
-class TestLoginAndRegister(TestCase):
-    """ Create a dummy login and go through all possible events for login and register."""
-    
