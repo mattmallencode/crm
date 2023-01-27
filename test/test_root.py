20b437cdc970e432d1e126ed6884eaf31151de25
OliverLinger <olienew32@gmail.com>
2023-01-27 14:28:36 +0000

    editing the test so that  landing page is login

--- test/test_root.py
+++ test/test_root.py
@@ -2,16 +2,15 @@ from test import client
 
 
 def test_landing(client):
-    landing = client.get("/")
+    landing = client.get("/login")
     html = landing.data.decode()
-
-    # Check that links to `about` and `login` pages exist
+    
     assert " <a href=\"/signup/\">signup</a>" in html
 
 
     assert landing.status_code == 200
 
 
-def test_landing_aliases(client):
+'''def test_landing_aliases(client):
     landing = client.get("/")
-    assert client.get("/index/").data == landing.data
+    assert client.get("/").data == landing.data'''
