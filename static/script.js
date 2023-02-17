5f8c29f23f7582ef93311f7019e89c6019044c0f
mattmallencode <72216955+mattmallencode@users.noreply.github.com>
2023-02-17 22:22:38 +0000

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-function save_timezone() {
-    let data = new FormData();
-    data.append("time_zone", Intl.DateTimeFormat().resolvedOptions().timeZone);
-    xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://127.0.0.1:5000/save_timezone");
-    xhttp.send(data);
-}
